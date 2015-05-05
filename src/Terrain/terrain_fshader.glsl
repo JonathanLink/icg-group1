cd2820be1905d472194085c8d2e4766bddf5b78e
Pierre Walch <pwalch@gmail.com>
2015-05-05 20:51:30 +0200

    Continue debugging

--- src/Terrain/terrain_fshader.glsl
+++ src/Terrain/terrain_fshader.glsl
@@ -1,23 +1,11 @@
 #version 330 core
 
 in float my_height;
-in vec3 my_pos;
-in vec3 friend_pos_1;
-in vec3 friend_pos_2;
+in float diffuse_coefficient;
 
 out vec3 color;
 
-uniform mat4 normal_matrix;
-
 void main() {
-    vec3 normal = normalize(
-        normal_matrix *
-        vec4(cross(friend_pos_1 - my_pos, friend_pos_2 - my_pos), 0)
-    ).xyz;
-    vec3 light_pos = vec3(0.0, 0.99, 0.0);
-    vec3 light = normalize(my_pos - light_pos);
-    float diffuse_coefficient = max(0.0, dot(normal, light));
-
     if (my_height <= 0.39) {
     	 color = vec3(0.1, 0.01, 1.0) * diffuse_coefficient;
     } else if (my_height <= 0.47) {
--- src/Terrain/terrain_vshader.glsl
+++ src/Terrain/terrain_vshader.glsl
@@ -2,30 +2,38 @@
 
 layout (location = 0) in vec2 position; 
 
+out float diffuse_coefficient;
 out float my_height;
-out vec3 my_pos;
-out vec3 friend_pos_1;
-out vec3 friend_pos_2;
 
 uniform mat4 MVP;
 uniform sampler2D tex;
+uniform mat4 normal_matrix;
 
 void main() {
 	// UV textures in [0,1] whereas OpenGL [-1, 1]
     vec2 my_uv_coords = (position + vec2(1.0, 1.0)) * 0.5;
-
 	float local_height = texture(tex, my_uv_coords).r;
 	vec3 local_pos_3d = vec3(position.x, local_height, -position.y);
 
-	float delta = 0.1;
-	vec2 east_move = vec2(delta, delta);
-	vec2 west_move = -east_move;
-	float friend_height_west = texture(tex, my_uv_coords + west_move).r;
-	float friend_height_east = texture(tex, my_uv_coords + east_move).r;
+	const vec2 size = vec2(2.0,0.0);
+	const ivec3 offset = ivec3(-1, 0, 1);
+	float s01 = textureOffset(tex, my_uv_coords, offset.xy).r;
+	float s21 = textureOffset(tex, my_uv_coords, offset.zy).r;
+	float s10 = textureOffset(tex, my_uv_coords, offset.yx).r;
+	float s12 = textureOffset(tex, my_uv_coords, offset.yz).r;
+    vec3 va = normalize(vec3(size.xy, s21 - s01));
+    vec3 vb = normalize(vec3(size.yx, s12 - s10));
+	vec3 vertex_normal = vec4(cross(va, vb), 0).xyz;
 
-    gl_Position = MVP * vec4(local_pos_3d, 1.0);
+	vec3 diffuse_reflection_normal = normalize(
+		normal_matrix *
+		vec4(vertex_normal.x, vertex_normal.y, vertex_normal.z, 0.0)
+	).xyz;
+
+	vec3 light_pos = vec3(0.0, 0.99, 0.0);
+	vec3 light_direction = normalize(local_pos_3d - light_pos);
+	diffuse_coefficient = max(0.0, dot(diffuse_reflection_normal, light_direction));
+
 	my_height = local_height;
-    my_pos = local_pos_3d;
-    friend_pos_1 = vec3(local_pos_3d.x + east_move.x, friend_height_east, local_pos_3d.z + east_move.y);
-	friend_pos_2 = vec3(local_pos_3d.x + west_move.x, friend_height_west, local_pos_3d.z + west_move.y);
+    gl_Position = MVP * vec4(local_pos_3d, 1.0);
 }
