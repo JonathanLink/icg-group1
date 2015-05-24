4438ca33c7075c4a74e863ad2b85e889e9fa246f
Pierre Walch <pwalch@gmail.com>
2015-05-24 19:48:59 +0200

    Better color for rain.

--- src/Particles/particles_fshader.glsl
+++ src/Particles/particles_fshader.glsl
@@ -1,7 +1,8 @@
 #version 330 core
 
+in float height;
 out vec4 color;
 
 void main() {
-    color = vec4(0.0f, 0.8f, 1.0f, 1.0f);
+    color = vec4(0.7f, 0.7f, 1.0f, 0.9f);
 }
--- src/Particles/particles_vshader.glsl
+++ src/Particles/particles_vshader.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 
 in vec3 position;
+out float height;
+
 uniform mat4 current_model;
 uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
+    height = position.y;
     gl_Position = projection * view * current_model * vec4(position, 1.0);
 }
